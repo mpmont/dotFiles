5e00c84c423612da04690189a73fe586ed12385d
Marco Monteiro <marco@marcomonteiro.net>
2020-04-19 17:39:59 +0100

    fish config

---
+++ .config/fish/conf.d/omf.fish
@@ -0,0 +1,15 @@
+# Path to Oh My Fish install.
+set -q XDG_DATA_HOME
+  and set -gx OMF_PATH "$XDG_DATA_HOME/omf"
+  or set -gx OMF_PATH "$HOME/.local/share/omf"
+
+# Load Oh My Fish configuration.
+source $OMF_PATH/init.fish
+
+alias config='/usr/bin/git --git-dir=$HOME/Documents/Github/dotfiles/ --work-tree=$HOME'
+alias ls='exa --long --header --git'
+alias dup='sudo docker-compose up -d --build'
+alias dps='sudo docker-compose ps'
+alias dstop='sudo docker-compose stop'
+
+figlet welcome Marco
