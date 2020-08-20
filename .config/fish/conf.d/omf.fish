8ed26439869f32566d6e0fab064fc28021f1c8c7
Marco Monteiro <marco@marcomonteiro.net>
2020-08-20 21:39:13 +0100

    added a bunch of alias to fish shell

--- .config/fish/conf.d/omf.fish
+++ .config/fish/conf.d/omf.fish
@@ -7,11 +7,58 @@ set -q XDG_DATA_HOME
 source $OMF_PATH/init.fish
 
 alias config='/usr/bin/git --git-dir=$HOME/Documents/Github/dotfiles/ --work-tree=$HOME'
-alias ls='exa --long --header --git'
+
+# ALIAS
+alias doas="doas --"
+
+# navigation
+alias ..='cd ..' 
+alias ...='cd ../..'
+alias .3='cd ../../..'
+alias .4='cd ../../..'
+alias .5='cd ../../../..'
+alias .6='cd ../../../../..'
+
+# Changing "ls" to "exa"
+alias ls='exa -al --color=always --group-directories-first' # my preferred listing
+alias la='exa -a --color=always --group-directories-first'  # all files and dirs
+alias ll='exa -l --color=always --group-directories-first'  # long format
+alias lt='exa -aT --color=always --group-directories-first' # tree listing
+alias l.='exa -a | egrep "^\."'
+
+# Colorize grep output (good for log files)
+alias grep='grep --color=auto'
+alias egrep='egrep --color=auto'
+alias fgrep='fgrep --color=auto'
+
+# adding flags
+alias cp="cp -i"                          # confirm before overwriting something
+alias df='df -h'                          # human-readable sizes
+alias free='free -m'                      # show sizes in MB
+
+# git
+alias addup='git add -u'
+alias addall='git add .'
+alias branch='git branch'
+alias checkout='git checkout'
+alias commit='git commit -m'
+alias fetch='git fetch'
+alias pull='git pull origin'
+alias push='git push origin'
+alias status='git status'
+alias tag='git tag'
+alias newtag='git tag -a'
+alias merge='git merge'
+
+# switch between shells
+alias tobash="sudo chsh $USER -s /bin/bash && echo 'Now log out.'"
+alias tozsh="sudo chsh $USER -s /bin/zsh && echo 'Now log out.'"
+alias tofish="sudo chsh $USER -s /bin/fish && echo 'Now log out.'"
+
+# Docker
 alias dup='sudo docker-compose up -d --build'
 alias dps='sudo docker-compose ps'
 alias dstop='sudo docker-compose stop'
-alias fuck='thefuck --alias | source'
 
 
 figlet welcome Marco
